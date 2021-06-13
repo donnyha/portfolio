4386a24176e70f1fb4263ba023ae65c5d225e610
Donny <donnyha@ubuntu.hitronhub.home>
2021-06-13 13:15:32 -0500

    html carried over

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+    return (
+        <div className="App">
+            <h1>Menu Decider</h1>
+
+                {/* Display Week */}
+                <table>
+
+                    {/* Days of the week */}
+                    <tr>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                        <th>Sunday</th>
+                    </tr>
+
+                    {/* Menu for the day */}
+                    <tr>
+                        <td class="day"></td>
+                        <td class="day"></td>
+                        <td class="day"></td>
+                        <td class="day"></td>
+                        <td class="day"></td>
+                        <td class="day"></td>
+                        <td class="day"></td>
+                    </tr>
+
+                </table>
+
+                {/* Display Menu */}
+                <div id="menu"><strong>This is Menu</strong><br /></div>
+
+                {/* Button */}
+                <button id="mix">Randomise</button>
+        </div>
+    );
 }
 
 export default App;

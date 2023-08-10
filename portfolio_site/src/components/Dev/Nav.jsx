8b16f5fe9bf5f366b054992ab0dd63b1a1fcc812
donnyha <donny.dh.ha@gmail.com>
2023-08-10 18:21:34 -0500

    scaffold styles

--- portfolio_site/src/components/Dev/Nav.jsx
+++ portfolio_site/src/components/Dev/Nav.jsx
@@ -1,16 +1,29 @@
+import { ReactComponent as SiteLogo } from './../../images/svg/logo-dh.svg';
+import { ReactComponent as GitHubLogo } from './../../images/svg/github.svg';
+import { ReactComponent as LinkedInLogo } from './../../images/svg/linkedin.svg';
+import { ReactComponent as ResumeLogo } from './../../images/svg/resume.svg';
+
 export default function Nav() {
   return (
-    <nav id="navbar">
-        <a href="#" id="logo-link"><img id="logo" src="./images/svg/logo-dh.svg" data-aos="fade-down" height="50px"></a>
-        <ul>
-            <li data-aos="fade-down" data-aos-delay="100"><a href="#about-me">about me</a></li>
-            <li data-aos="fade-down" data-aos-delay="200"><a href="#projects">projects</a></li>
-            <li data-aos="fade-down" data-aos-delay="300"><a href="#contact">contact</a></li>
+    <nav id="navbar" className="bg-nav-bg shadow-nav-shadow h-16 transition-top grid grid-cols-nav-gridCols fixed top-0 w-full z-50">
+        <a href="/dev" id="logo-link">
+          <SiteLogo data-aos="fade-down" />
+        </a>
+        <ul className="flex justify-evenly items-center list-none m-0">
+            <li data-aos="fade-down" data-aos-delay="100" className="flex items-center"><a href="#about-me" className="text-white no-underline font-bold text-lg transition-colors hover:text-pink-500">about me</a></li>
+            <li data-aos="fade-down" data-aos-delay="200" className="flex items-center"><a href="#projects">projects</a></li>
+            <li data-aos="fade-down" data-aos-delay="300" className="flex items-center"><a href="#contact">contact</a></li>
         </ul>
-        <span class="social">
-            <a href="https://github.com/donnyha" target="_blank" data-aos="fade-down" data-aos-delay="400"><img class="responsive" src="/images/svg/github.svg" /></a>
-            <a href="https://www.linkedin.com/in/donnyha/" target="_blank" data-aos="fade-down" data-aos-delay="500"><img class="responsive" src="/images/svg/linkedin.svg" /></a>
-            <a href="./documents/donnyha_resume.pdf" target="_blank" data-aos="fade-down" data-aos-delay="600"><img id="email" class="responsive" src="/images/svg/resume.svg" /></a>
+        <span className="social">
+            <a href="https://github.com/donnyha" target="_blank" rel="noreferrer" data-aos="fade-down" data-aos-delay="400">
+              <GitHubLogo />
+            </a>
+            <a href="https://www.linkedin.com/in/donnyha/" target="_blank" rel="noreferrer" data-aos="fade-down" data-aos-delay="500">
+              <LinkedInLogo />
+            </a>
+            <a href="./documents/donnyha_resume.pdf" target="_blank" rel="noreferrer" data-aos="fade-down" data-aos-delay="600">
+              <ResumeLogo />
+            </a>
         </span>
     </nav>
   ) 
--- portfolio_site/src/pages/Dev.jsx
+++ portfolio_site/src/pages/Dev.jsx
@@ -2,7 +2,7 @@ import Nav from './../components/Dev/Nav';
 
 export default function Dev () {
   return (
-    <div>
+    <div className="h-screen text-white bg-[#1b1b32] font-body antialiased scrolling-touch">
       <Nav />
       <h1>Dev Home Here</h1>
     </div>

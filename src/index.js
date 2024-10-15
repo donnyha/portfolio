d8c9169ecbe3642dc789c1e118eab38706e71e64
donnyha4 <donny@avenue4.com>
2024-10-15 16:29:28 -0500

    implement tag for GA4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import { RouterProvider } from "react-router-dom";
 import { router } from './router';
 
+const googleAnalyticsId = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
+
+if (googleAnalyticsId) {
+  const script = document.createElement('script');
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', googleAnalyticsId);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
